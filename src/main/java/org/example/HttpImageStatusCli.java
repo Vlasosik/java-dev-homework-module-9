94f86f7b24d8987582b0604e65529f5178cac87f
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-01-14 12:22:38 +0200

    created class HttpImageStatusCli and void method  askStatus

---
+++ src/main/java/org/example/HttpImageStatusCli.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.utilException.ImageException;
+
+import java.util.Scanner;
+
+public class HttpImageStatusCli {
+    void askStatus() {
+        HttpStatusImageDownloader httpStatusImageDownloader = new HttpStatusImageDownloader();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter HTTP status code: ");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int code = scanner.nextInt();
+                try {
+                    httpStatusImageDownloader.downloadStatusImage(code);
+                    System.out.println("Image download successful!");
+                    break;
+                } catch (ImageException ex) {
+                    System.out.println("Here is not image for HTTP status: " + code);
+                    System.out.println("Please enter the code again to get the picture:");
+                }
+            } else {
+                System.out.println("Please enter valid number: ");
+                scanner.next();
+            }
+        }
+        scanner.close();
+    }
+}
