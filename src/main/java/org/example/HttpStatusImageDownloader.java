d9051f3f4989b36c3463ddf1cb275cb19a6932eb
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-01-14 12:21:54 +0200

    created class HttpStatusImageDownloader and method downloadStatusImage

---
+++ src/main/java/org/example/HttpStatusImageDownloader.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.example.utilException.ImageException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HttpStatusImageDownloader {
+    void downloadStatusImage(int code) throws ImageException {
+        String url = new HttpStatusChecker().getStatusImage(code);
+        if (url == null) {
+            throw new ImageException("Image not found for the status code: " + code);
+        }
+        try {
+            Files.createDirectories(Paths.get("src/main/java/org/example/img"));
+            HttpClient client = HttpClient.newBuilder()
+                    .followRedirects(HttpClient.Redirect.NORMAL)
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(new HttpStatusChecker().getStatusImage(code)))
+                    .GET()
+                    .build();
+            String directory = "src/main/java/org/example/img/";
+            String fileName = "image_status_" + code + ".jpg";
+            HttpResponse<Path> send = client.send(request, HttpResponse.BodyHandlers.ofFile(Path.of(directory, fileName)));
+            if (send.statusCode() != 200) {
+                throw new ImageException("Failed to download the image for the status code: " + code);
+            }
+        } catch (IOException | URISyntaxException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
