baae1a1fd267687b609ec7d606205197aad30144
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-01-14 12:24:00 +0200

    created class Main and void main method to run our program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        HttpImageStatusCli httpImageStatusCli = new HttpImageStatusCli();
+        httpImageStatusCli.askStatus();
+    }
+}
